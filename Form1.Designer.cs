4703b864ac48dede2b7a69fc328e66cbc2fea2d2
georgebreje <george.breje2@gmail.com>
2021-07-27 17:04:58 +0300

    resize one border when forms gets resized

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,38 @@ namespace ChessGame
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1066, 548);
+            this.panel1.TabIndex = 0;
+            this.panel1.SizeChanged += new System.EventHandler(this.panel1_SizeChanged);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1090, 572);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,62 @@ namespace ChessGame
 {
     public partial class Form1 : Form
     {
+        public float Latime { get; set; }
+        public float Lungime { get; set; }
+        public float BorderH { get; set; }
+        public float BorderW { get; set; }
+        PointF[] points = new PointF[4];
+        Graphics grp;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            var p = sender as Panel;
+            var g = e.Graphics;
+
+
+            g.FillRectangle(new SolidBrush(Color.Black), p.DisplayRectangle);
+
+            Brush brush = new SolidBrush(Color.DarkGreen);
+
+            g.FillPolygon(brush, points);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_SizeChanged(object sender, EventArgs e)
+        {
+            var p = sender as Panel;
+            grp = panel1.CreateGraphics();
+            Refresh(); 
+            Latime = p.Height;
+            Lungime = p.Width;
+            BorderW = Lungime / 8;
+            BorderH = Latime / 8;
+            points[0] = new PointF(0, 0);
+            points[1] = new PointF(0, BorderH);
+            points[2] = new PointF(BorderW, BorderH);
+            points[3] = new PointF(BorderW, 0);
+            Redraw(grp);
+        }
+
+        private void Redraw(Graphics g)
+        {
+            Brush brush = new SolidBrush(Color.DarkGreen);
+
+            g.FillPolygon(brush, points);
+        }
+
+        private void Refresh(Graphics g)
+        {
+            g.FillPolygon(new SolidBrush(Color.Black), points);
         }
     }
 }
