cdc7dce229543d518e4562ebd2c7a6d712d5b363
georgebreje <george.breje2@gmail.com>
2021-07-28 19:55:47 +0300

    filling a row with fileds that are modifying their size if Forms modifies

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,8 @@ namespace ChessGame
         public float BorderH { get; set; }
         public float BorderW { get; set; }
         PointF[] points = new PointF[4];
+        PointF[] pointz = new PointF[4];
+
         Graphics grp;
         
         public Form1()
@@ -28,19 +30,29 @@ namespace ChessGame
         {
             var p = sender as Panel;
             var g = e.Graphics;
-
-
-            g.FillRectangle(new SolidBrush(Color.Black), p.DisplayRectangle);
-
-            Brush brush = new SolidBrush(Color.DarkGreen);
-
-            g.FillPolygon(brush, points);
+            float a = 0;
+            float b = 0;
+            
+            for (int i = 1; i <= 8; i++)
+            {
+                points[0] = new PointF(b, 0);
+                points[1] = new PointF(b, BorderH);
+                points[2] = new PointF(b + BorderW, BorderH);
+                points[3] = new PointF(b + BorderW, 0);
+                if (i % 2 != 0)
+                    Redraw(grp, Color.SaddleBrown);
+                else
+                    Redraw(grp, Color.Chocolate);
+                b += BorderW;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        public static Random rnd = new Random();
 
         private void panel1_SizeChanged(object sender, EventArgs e)
         {
@@ -51,16 +63,68 @@ namespace ChessGame
             Lungime = p.Width;
             BorderW = Lungime / 8;
             BorderH = Latime / 8;
-            points[0] = new PointF(0, 0);
-            points[1] = new PointF(0, BorderH);
-            points[2] = new PointF(BorderW, BorderH);
-            points[3] = new PointF(BorderW, 0);
-            Redraw(grp);
+            float a = 0;
+            float b = 0;
+
+            for (int i = 1; i <= 8; i++)
+            {
+                points[0] = new PointF(b, 0);
+                points[1] = new PointF(b, BorderH);
+                points[2] = new PointF(b + BorderW, BorderH);
+                points[3] = new PointF(b + BorderW, 0);
+
+                if (i % 2 != 0)
+                    Redraw(grp, Color.SaddleBrown);
+                else
+                    Redraw(grp, Color.Chocolate);
+
+                b += BorderW;
+            }
+            //pointz[0] = new PointF(BorderW, 0);
+            //pointz[1] = new PointF(BorderW, BorderH);
+            //pointz[2] = new PointF(BorderW + BorderW, BorderH);
+            //pointz[3] = new PointF(BorderW + BorderW, 0);
+
+            //grp.FillPolygon(new SolidBrush(Color.Black), pointz);
+
+
+            //float a = 0;
+            //for (int i = 1; i <= 8; i++)  
+            //{
+            //    float b = 0;
+            //    for (int j = 1; j <= 8; j++)
+            //    {
+            //        if (i % 2 != 0)
+            //        {
+            //            if (j % 2 != 0)
+            //            {
+            //                Redraw(grp, Color.SaddleBrown);
+            //            }
+            //            else
+            //                Redraw(grp, Color.White);
+            //        }
+            //        else
+            //        {
+
+            //        }
+            //        b += BorderW;
+            //    }
+            //    a += BorderH;
+            //}
+
+            //Redraw(grp);
+        }
+
+        private void Redraw(Graphics g, Color c)
+        {
+            Brush brush = new SolidBrush(c);
+
+            g.FillPolygon(brush, points);
         }
 
         private void Redraw(Graphics g)
         {
-            Brush brush = new SolidBrush(Color.DarkGreen);
+            Brush brush = new SolidBrush(Color.SaddleBrown);
 
             g.FillPolygon(brush, points);
         }
